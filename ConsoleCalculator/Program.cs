412baa89d478afebe92bc83d1a422a44aad1dc83
Dylan Townes <dylanrtownes@outlook.com>
2022-10-28 10:42:05 -0500

    Removed comments

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -8,19 +8,16 @@ class Program
     static void Main(string[] args)
     {
         bool endApp = false;
-        // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
         Calculator calculator = new Calculator();
         while (!endApp)
         {
-            // Declare variables and set to empty.
             string numInput1 = "";
             string numInput2 = "";
             double result = 0;
 
-            // Ask the user to type the first number.
             Console.Write("Type a number, and then press Enter: ");
             numInput1 = Console.ReadLine();
 
@@ -30,8 +27,6 @@ class Program
                 Console.Write("This is not valid input. Please enter an integer value: ");
                 numInput1 = Console.ReadLine();
             }
-
-            // Ask the user to type the second number.
             Console.Write("Type another number, and then press Enter: ");
             numInput2 = Console.ReadLine();
 
@@ -41,8 +36,6 @@ class Program
                 Console.Write("This is not valid input. Please enter an integer value: ");
                 numInput2 = Console.ReadLine();
             }
-
-            // Ask the user to choose an operator.
             Console.WriteLine("Choose an operator from the following list:");
             Console.WriteLine("\ta - Add");
             Console.WriteLine("\ts - Subtract");
@@ -68,11 +61,10 @@ class Program
 
             Console.WriteLine("------------------------\n");
 
-            // Wait for the user to respond before closing.
             Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
             if (Console.ReadLine() == "n") endApp = true;
 
-            Console.WriteLine("\n"); // Friendly linespacing.
+            Console.WriteLine("\n"); 
         }
         calculator.Finish();
         return;
